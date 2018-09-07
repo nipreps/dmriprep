1a04c76f4d23b4968cecc71cd528165ab4b71404
akeshavan <keshavan@berkeley.edu>
2018-09-07 10:54:36 -0700

    wip/brk: trying to see if adding a /preafq/ will load the svg logo

--- preafqViewer/config/index.js
+++ preafqViewer/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/preAFQ',
+    assetsPublicPath: '/preAFQ/',
 
     /**
      * Source Maps
