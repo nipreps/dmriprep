21df2b0ddcdacb56ebfaa4258c422e8a5715ed9b
Adam Richie-Halford <richford@uw.edu>
2018-12-10 06:26:36 -0800

    Add eddy-niter and slice-outlier-threshold to the CLI args

--- dmriprep/cli.py
+++ dmriprep/cli.py
@@ -11,13 +11,24 @@ import os
 
 @click.command()
 @click.option('--participant-label', help="The label(s) of the participant(s) that should be"
-                                           "analyzed. The label corresponds to"
-                                           "sub-<participant_label> from the BIDS spec (so it does"
-                                           "not include 'sub-'). If this parameter is not provided"
-                                           "all subjects will be analyzed. Multiple participants"
-                                           "can be specified with a space separated list.",
+                                          "analyzed. The label corresponds to"
+                                          "sub-<participant_label> from the BIDS spec (so it does"
+                                          "not include 'sub-'). If this parameter is not provided"
+                                          "all subjects will be analyzed. Multiple participants"
+                                          "can be specified with a space separated list.",
               default=None
               )
+@click.option('--eddy-niter',
+              help="Fixed number of eddy iterations. See"
+                   "https://fsl.fmrib.ox.ac.uk/fsl/fslwiki/eddy/UsersGuide#A--niter",
+              default=5)
+@click.option('--slice-outlier-threshold',
+              help="Number of allowed outlier slices per volume."
+                   "If this is exceeded the volume is dropped from analysis. If"
+                   "`slice_outlier_threshold` is an int, it is treated as number of allowed"
+                   "outlier slices. If `slice_outlier_threshold` is a float between 0 and 1"
+                   "(exclusive), it is treated the fraction of allowed outlier slices.",
+              default=0.02)
 @click.argument('bids_dir',
                 )
 @click.argument('output_dir',
@@ -25,7 +36,9 @@ import os
 @click.argument('analysis_level',
                 type=click.Choice(['participant', 'group']),
                 default='participant')
-def main(participant_label, bids_dir, output_dir, analysis_level="participant"):
+def main(participant_label, bids_dir, output_dir,
+         eddy_niter=5, slice_outlier_threshold=0.02,
+         analysis_level="participant"):
     """
     BIDS_DIR: The directory with the input dataset formatted according to the BIDS standard.
 
@@ -38,7 +51,6 @@ def main(participant_label, bids_dir, output_dir, analysis_level="participant"):
     participant level analyses can be run independently
     (in parallel).
     """
-
     if analysis_level is not 'participant':
         raise NotImplementedError('The only valid analysis level for dmriprep is participant at the moment.')
 
@@ -47,7 +59,9 @@ def main(participant_label, bids_dir, output_dir, analysis_level="participant"):
     for subject_inputs in inputs:
         run.run_dmriprep_pe(**subject_inputs,
                             working_dir=os.path.join(output_dir, 'scratch'),
-                            out_dir=output_dir)
+                            out_dir=output_dir,
+                            eddy_niter=eddy_niter,
+                            slice_outlier_threshold=slice_outlier_threshold)
 
     return 0
 
