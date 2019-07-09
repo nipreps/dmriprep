dbee40dd5ba4ee912da9ea2a37452e790f418eb4
Michael Joseph <josephmje.22@gmail.com>
2019-07-09 06:26:07 -0400

    add topup workflow

--- dmriprep/workflows/fieldmap/__init__.py
+++ dmriprep/workflows/fieldmap/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
 from .base import init_sdc_prep_wf
+from .pepolar import init_pepolar_wf
 from .fmap import init_fmap_wf
 from .phasediff import init_phase_wf, init_phdiff_wf
