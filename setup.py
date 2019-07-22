a48b73f8417cefabc2c2f3dfe4aed845d4eab905
Michael Joseph <josephmje.22@gmail.com>
2019-07-22 17:23:39 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -30,37 +30,44 @@ setup_requirements = ["pytest-runner"]
 test_requirements = ["pytest"]
 
 setup(
-    author="Anisha Keshavan",
-    author_email="keshavan@berkeley.edu",
+    # project information
+    name="dmripreproc",
+    version="0.1.0",
+    author="Michael Joseph",
+    author_email="michael.joseph@camh.ca",
+    url="https://github.com/tigrlab/dmripreproc",
+    license="BSD license",
+    # description
+    description="Preprocessing of diffusion MRI data",
+    long_description=readme + "\n\n" + history,
+    # requirements
+    python_requires=">=3.5",
+    install_requires=requirements,
+    setup_requires=setup_requirements,
+    extras_require=extras_require,
+    # packaging
+    packages=find_packages(include=["dmripreproc*"]),
+    include_package_data=True,
+    zip_safe=False,
+    # tests
+    test_suite="tests",
+    tests_require=test_requirements,
+    # cli
+    entry_points={"console_scripts": ["dmripreproc=dmripreproc.cli:main"]},
+    # metadata
+    keywords="diffusion bids",
+    project_urls={
+        "Documentation": "https://dmripreproc.readthedocs.io/en/latest/",
+        "Tracker": "https://github.com/TIGRLab/dmripreproc/issues/",
+    },
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
-    description="Preprocessing of neuroimaging data in preparation for AFQ analysis",
-    entry_points={
-        "console_scripts": [
-            "dmripreproc=dmripreproc.cli:main",
-            "dmripreproc-data=dmripreproc.cli:data",
-            "dmripreproc-upload=dmripreproc.cli:upload",
-        ]
-    },
-    install_requires=requirements,
-    extras_require=extras_require,
-    license="BSD license",
-    long_description=readme + "\n\n" + history,
-    include_package_data=True,
-    keywords="dmripreproc",
-    name="dmripreproc",
-    packages=find_packages(include=["dmripreproc*"]),
-    setup_requires=setup_requirements,
-    test_suite="tests",
-    tests_require=test_requirements,
-    url="https://github.com/tigrlab/dmripreproc",
-    version="0.1.0",
-    zip_safe=False,
 )
