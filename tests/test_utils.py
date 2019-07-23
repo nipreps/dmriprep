d03821529f42fd05e0b3471689a6fc56dc8efc66
mjoseph <josephmje.22@gmail.com>
2019-07-23 17:24:38 -0400

    update tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -23,23 +23,29 @@ def uniform_points_on_sphere(npoints=1, hemisphere=True, rotate=(0, 0, 0)):
 
     vecs = np.stack([x, y, z])
 
-    rot_x = np.array([
-        [1.0, 0.0, 0.0],
-        [0.0, np.cos(rotate[0]), -np.sin(rotate[0])],
-        [0.0, np.sin(rotate[0]), np.cos(rotate[0])]
-    ])
+    rot_x = np.array(
+        [
+            [1.0, 0.0, 0.0],
+            [0.0, np.cos(rotate[0]), -np.sin(rotate[0])],
+            [0.0, np.sin(rotate[0]), np.cos(rotate[0])],
+        ]
+    )
 
-    rot_y = np.array([
-        [np.cos(rotate[1]), 0.0, np.sin(rotate[1])],
-        [0.0, 1.0, 0.0],
-        [-np.sin(rotate[1]), 0.0, np.cos(rotate[1])]
-    ])
+    rot_y = np.array(
+        [
+            [np.cos(rotate[1]), 0.0, np.sin(rotate[1])],
+            [0.0, 1.0, 0.0],
+            [-np.sin(rotate[1]), 0.0, np.cos(rotate[1])],
+        ]
+    )
 
-    rot_z = np.array([
-        [np.cos(rotate[2]), -np.sin(rotate[2]), 0.0],
-        [np.sin(rotate[2]), np.cos(rotate[2]), 0.0],
-        [0.0, 0.0, 1.0]
-    ])
+    rot_z = np.array(
+        [
+            [np.cos(rotate[2]), -np.sin(rotate[2]), 0.0],
+            [np.sin(rotate[2]), np.cos(rotate[2]), 0.0],
+            [0.0, 0.0, 1.0],
+        ]
+    )
 
     vecs = np.dot(rot_z, np.dot(rot_y, np.dot(rot_x, vecs)))
 
@@ -50,9 +56,11 @@ def test_is_hemispherical():
     vecs = uniform_points_on_sphere(
         npoints=100,
         hemisphere=True,
-        rotate=(np.random.uniform(0, np.pi),
-                np.random.uniform(0, np.pi),
-                np.random.uniform(0, np.pi))
+        rotate=(
+            np.random.uniform(0, np.pi),
+            np.random.uniform(0, np.pi),
+            np.random.uniform(0, np.pi),
+        ),
     )
 
     assert is_hemispherical(vecs)[0]
