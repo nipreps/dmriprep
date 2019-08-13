03a51487681b3b7a30c763c665942c4f0afe9d00
mjoseph <josephmje.22@gmail.com>
2019-08-13 17:00:09 -0400

    calculate RD

--- dmriprep/workflows/dwi/tensor.py
+++ dmriprep/workflows/dwi/tensor.py
@@ -16,18 +16,17 @@ def init_tensor_wf():
     )
 
     outputnode = pe.Node(
-        niu.IdentityInterface(fields=[
-            "FA_file",
-            "L1_file",
-            "L2_file",
-            "L3_file",
-            "MD_file",
-            "sse_file",
-            "V1_file"]),
+        niu.IdentityInterface(
+            fields=["FA_file", "MD_file", "AD_file", "RD_file", "sse_file", "V1_file"]
+        ),
         name="outputnode",
     )
 
     dtifit = pe.Node(fsl.DTIFit(save_tensor=True, sse=True), name="dtifit")
+
+    add_l2_l3 = pe.Node(fsl.BinaryMaths(operation="add"), name="add_l2_l3")
+
+    calc_rd = pe.Node(fsl.BinaryMaths(operand_value=2, operation="div"), name="calc_rd")
 
     wf.connect(
         [
@@ -41,16 +40,19 @@ def init_tensor_wf():
                     ("mask_file", "mask"),
                 ],
             ),
+            (dtifit, add_l2_l3, [("L2", "in_file"), ("L3", "operand_file")]),
+            (add_l2_l3, calc_rd, [("out_file", "in_file")]),
+            (calc_rd, outputnode, [("out_file", "RD_file")]),
             (
                 dtifit,
                 outputnode,
-                [("FA", "FA_file"),
-                 ("L1", "L1_file"),
-                 ("L2", "L2_file"),
-                 ("L3", "L3_file"),
-                 ("MD", "MD_file"),
-                 ("sse", "sse_file"),
-                 ("V1", "V1_file")],
+                [
+                    ("FA", "FA_file"),
+                    ("MD", "MD_file"),
+                    ("L1", "AD_file"),
+                    ("sse", "sse_file"),
+                    ("V1", "V1_file"),
+                ],
             ),
         ]
     )
