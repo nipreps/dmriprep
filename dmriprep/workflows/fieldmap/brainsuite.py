5d472a4ae019a1560f01c370d7c117ec0198f4a5
Michael Joseph <josephmje.22@gmail.com>
2019-09-04 07:15:37 -0400

    wip add brainsuite

--- dmriprep/workflows/fieldmap/brainsuite.py
+++ dmriprep/workflows/fieldmap/brainsuite.py
@@ -1,6 +1,40 @@
+# -*- coding: utf-8 -*-
 
+"""
+SDC using BrainSuite
+^^^^^^^^^^^^^^^^^^^^
 
-# bfc -i $indir2/brain.nii.gz -o T1.bfc.nii.gz -v 1 -L 0.5 -U 1.5
+.. autofunction:: init_brainsuite_wf
+
+"""
+
+from nipype.pipeline import engine as pe
+from nipype.interfaces import brainsuite, utility as niu
+
+def init_fmap_wf():
+
+    wf = pe.Workflow(name='brainsuite_wf')
+
+    inputnode = pe.Node(
+        niu.IdentityInterface(
+            fields=[
+                'T1w',
+                'T1w_mask',
+                'dwi_file',
+                'dwi_mask']),
+        name='inputnode')
+
+    outputnode = pe.Node(
+        niu.IdentityInterface(fields=['out_fmap']), name='outputnode'
+    )
+
+    bias_corr = pe.Node(brainsuite.Bfc(minBias=0.5, maxBias=1.5), name='bias_corr')
+
+    bdp = pe.Node(brainsuite.BDP(), name='bdp')
+
+    wf.connect([])
+
+# bfc -i $indir2/brain.nii.gz -o T1.bfc.nii.gz
 
 # /KIMEL/tigrlab/quarantine/brainsuite/18a/build/bdp/bdp.sh \
 #   T1.bfc.nii.gz \
