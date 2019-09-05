4e2feda1f77010f007ff5700282c922415fa9250
oesteban <code@oscaresteban.es>
2019-09-05 16:53:22 -0700

    maint: add crediting details

---
+++ dmriprep/__about__.py
@@ -0,0 +1,16 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""Base module variables."""
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
+__packagename__ = 'dmriprep'
+__copyright__ = 'Copyright 2019, The dMRIPrep developers'
+__credits__ = ('Contributors: please check the ``.zenodo.json`` file at the top-level folder'
+               'of the repository')
+__url__ = 'https://github.com/nipreps/dmriprep'
+
+DOWNLOAD_URL = (
+    'https://github.com/nipreps/{name}/archive/{ver}.tar.gz'.format(
+        name=__packagename__, ver=__version__))
--- dmriprep/__init__.py
+++ dmriprep/__init__.py
@@ -1,58 +1,23 @@
-# -*- coding: utf-8 -*-
-
 """Top-level package for dmriprep."""
-
-__author__ = """Anisha Keshavan"""
-__email__ = 'anishakeshavan@gmail.com'
-__version__ = '0.1.0'
-
-import errno
-import logging
-import os
-import warnings
-
-# Filter warnings that are visible whenever you import another package that
-# was compiled against an older numpy than is installed.
-warnings.filterwarnings("ignore", message="numpy.dtype size changed")
-warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
-
-from . import data
-from . import io
-from . import qc
-from . import run
-
-module_logger = logging.getLogger(__name__)
-
-# get the log level from environment variable
-if "DMIRPREP_LOGLEVEL" in os.environ:
-    loglevel = os.environ['DMRIPREP_LOGLEVEL']
-    module_logger.setLevel(getattr(logging, loglevel.upper()))
-else:
-    module_logger.setLevel(logging.WARNING)
-
-# create a file handler
-logpath = os.path.join(os.path.expanduser('~'), '.dmriprep', 'dmriprep.log')
-
-# Create the config directory if it doesn't exist
-logdir = os.path.dirname(logpath)
-try:
-    os.makedirs(logdir)
-except OSError as e:
-    pre_existing = (e.errno == errno.EEXIST and os.path.isdir(logdir))
-    if pre_existing:
-        pass
-    else:
-        raise e
-
-handler = logging.FileHandler(logpath, mode='w')
-handler.setLevel(logging.DEBUG)
-
-# create a logging format
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+import warnings as _warnings
+from .__about__ import (
+    __version__,
+    __copyright__,
+    __credits__,
+    __packagename__,
 )
-handler.setFormatter(formatter)
 
-# add the handlers to the logger
-module_logger.addHandler(handler)
-module_logger.info('Started new dmriprep session')
+
+__all__ = [
+    '__version__',
+    '__copyright__',
+    '__credits__',
+    '__packagename__',
+]
+
+# cmp is not used by fmriprep, so ignore nipype-generated warnings
+_warnings.filterwarnings('ignore', r'cmp not installed')
+_warnings.filterwarnings('ignore', r'This has not been fully tested. Please report any failures.')
+_warnings.filterwarnings('ignore', r"can't resolve package from __spec__ or __package__")
+_warnings.simplefilter('ignore', DeprecationWarning)
+_warnings.simplefilter('ignore', ResourceWarning)
