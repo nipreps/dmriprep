baec874163febf5500fee5f638a79782d3fe58f4
mjoseph <josephmje.22@gmail.com>
2019-12-04 12:51:14 -0500

    add pytest fixtures

--- dmriprep/conftest.py
+++ dmriprep/conftest.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import numpy as np
 import nibabel as nb
 import pytest
+from bids.layout import BIDSLayout
 from dipy.data.fetcher import _make_fetcher, UW_RW_URL
 
 _dipy_datadir_root = os.getenv('DMRIPREP_TESTS_DATA') or Path.home()
@@ -28,6 +29,20 @@ _sherbrooke_data = {
     'bvals': np.loadtxt(dipy_datadir / "HARDI193.bval"),
 }
 
+test_data_env = os.getenv('TEST_DATA_HOME', '/tmp/data')
+test_output_dir = os.getenv('TEST_OUTPUT_DIR')
+test_workdir = os.getenv('TEST_WORK_DIR')
+
+layouts = {p.name: BIDSLayout(str(p), validate=False, derivatives=True)
+           for p in Path(test_data_env).glob('*') if p.is_dir()}
+
+
+def pytest_report_header(config):
+    msg = "Datasets found: %s" % ', '.join([v.root for v in layouts.values()])
+    if test_output_dir is not None:
+        msg += '\nOutput folder: %s' % Path(test_output_dir).resolve()
+    return msg
+
 
 @pytest.fixture(autouse=True)
 def doctest_autoimport(doctest_namespace):
@@ -48,3 +63,18 @@ def doctest_autoimport(doctest_namespace):
 def dipy_test_data(scope='session'):
     """Create a temporal directory shared across tests to pull data in."""
     return _sherbrooke_data
+
+
+@pytest.fixture
+def workdir():
+    return None if test_workdir is None else Path(test_workdir)
+
+
+@pytest.fixture
+def output_path():
+    return None if test_output_dir is None else Path(test_output_dir)
+
+
+@pytest.fixture
+def bids_layouts():
+    return layouts
