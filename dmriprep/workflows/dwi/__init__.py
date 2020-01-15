83e86a93f1ad5a6e824cadb4b4eb11c30dfdf2e8
oesteban <code@oscaresteban.es>
2020-01-15 00:46:11 -0800

    sty: fix all dunder

--- dmriprep/workflows/dwi/__init__.py
+++ dmriprep/workflows/dwi/__init__.py
@@ -1,12 +1,9 @@
-"""
-Pre-processing dMRI workflows
-
-"""
+"""Pre-processing dMRI workflows."""
 
 from .base import init_dwi_preproc_wf
 from .util import init_dwi_reference_wf
 
-_all__ = [
+__all__ = [
     'init_dwi_preproc_wf',
-    'init_dwi_reference_wf'
+    'init_dwi_reference_wf',
 ]
