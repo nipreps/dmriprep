8405ac97bdde6d2637dc4ad4de5d50528f25852b
ci-build <nipreps@gmail.com>
2020-04-21 20:30:15 +0000

    doc(update) [skip ci]

--- 0.2.2/api/dmriprep-workflows-base-2.py
+++ 0.2.2/api/dmriprep-workflows-base-2.py
@@ -1,4 +1,4 @@
 from dmriprep.config.testing import mock_config
 from dmriprep.workflows.base import init_single_subject_wf
 with mock_config():
-    wf = init_single_subject_wf('THP0005')
+    wf = init_single_subject_wf("THP0005")
--- 0.2.2/api/dmriprep-workflows-dwi-base-1.py
+++ 0.2.2/api/dmriprep-workflows-dwi-base-1.py
@@ -1,6 +1,5 @@
 from dmriprep.config.testing import mock_config
 from dmriprep import config
-from dmriprep.workflows.dwi.base import init_dwi_preproc_wf
+from dmriprep.workflows.dwi.base import init_early_b0ref_wf
 with mock_config():
-    dwi_file = config.execution.bids_dir / 'sub-THP0005' / 'dwi'                     / 'sub-THP0005_dwi.nii.gz'
-    wf = init_dwi_preproc_wf(str(dwi_file))
+    wf = init_early_b0ref_wf()
