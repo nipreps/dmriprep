772aee2393cf62b04cadff23c74c788a27f91b87
ci-build <nipreps@gmail.com>
2020-12-10 15:42:55 +0000

    doc(update) [skip ci]

--- 0.3.0/api/dmriprep-workflows-dwi-base-1.py
+++ 0.3.0/api/dmriprep-workflows-dwi-base-1.py
@@ -1,5 +1,8 @@
 from dmriprep.config.testing import mock_config
 from dmriprep import config
-from dmriprep.workflows.dwi.base import init_early_b0ref_wf
+from dmriprep.workflows.dwi.base import init_dwi_preproc_wf
 with mock_config():
-    wf = init_early_b0ref_wf()
+    wf = init_dwi_preproc_wf(
+        f"{config.execution.layout.root}/"
+        "sub-THP0005/dwi/sub-THP0005_dwi.nii.gz"
+    )
