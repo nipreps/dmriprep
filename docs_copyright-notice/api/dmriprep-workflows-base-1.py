945bd6ac757419738218101f1506d9348b2072f6
ci-build <nipreps@gmail.com>
2021-05-19 07:17:36 +0000

    doc(update) [skip ci]

--- docs_copyright-notice/api/dmriprep-workflows-base-1.py
+++
@@ -1,4 +0,0 @@
-from dmriprep.config.testing import mock_config
-from dmriprep.workflows.base import init_dmriprep_wf
-with mock_config():
-    wf = init_dmriprep_wf()
--- docs_copyright-notice/api/dmriprep-workflows-base-2.py
+++
@@ -1,4 +0,0 @@
-from dmriprep.config.testing import mock_config
-from dmriprep.workflows.base import init_single_subject_wf
-with mock_config():
-    wf = init_single_subject_wf("THP0005")
