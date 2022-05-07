fb3bb9a6cfd44d18e430fc2650cb3742930b8b7d
GalBenZvi <hershkovitz1@mail.tau.ac.il>
2022-05-07 20:45:38 +0300

    WIP dwi workflow

--- dmriprep/workflows/dwi/base.py
+++ dmriprep/workflows/dwi/base.py
@@ -23,15 +23,20 @@
 """Orchestrating the dMRI-preprocessing workflow."""
 from pathlib import Path
 
+from dmriprep.workflows.dwi.utils import (
+    _aslist,
+    _get_wf_name,
+    extract_entities,
+)
 from nipype.interfaces import utility as niu
 from nipype.pipeline import engine as pe
-from niworkflows.utils.connections import listify
+
 
 from ... import config
 from ...interfaces import DerivativesDataSink
 
 
-def init_dwi_preproc_wf(dwi_file, has_fieldmap=False):
+def init_dwi_preproc_wf(dwi_file):
     """
     Build a preprocessing workflow for one DWI run.
 
@@ -137,14 +142,6 @@ def init_dwi_preproc_wf(dwi_file, has_fieldmap=False):
             fields=[
                 # DWI
                 "dwi_file",
-                "in_bvec",
-                "in_bval",
-                # From SDCFlows
-                "fmap",
-                "fmap_ref",
-                "fmap_coeff",
-                "fmap_mask",
-                "fmap_id",
                 # From anatomical
                 "t1w_preproc",
                 "t1w_mask",
@@ -164,8 +161,6 @@ def init_dwi_preproc_wf(dwi_file, has_fieldmap=False):
         name="inputnode",
     )
     inputnode.inputs.dwi_file = str(dwi_file.absolute())
-    inputnode.inputs.in_bvec = str(layout.get_bvec(dwi_file))
-    inputnode.inputs.in_bval = str(layout.get_bval(dwi_file))
 
     outputnode = pe.Node(
         niu.IdentityInterface(
@@ -173,8 +168,6 @@ def init_dwi_preproc_wf(dwi_file, has_fieldmap=False):
         ),
         name="outputnode",
     )
-
-    gradient_table = pe.Node(CheckGradientTable(), name="gradient_table")
 
     dwi_reference_wf = init_epi_reference_wf(
         omp_nthreads=config.nipype.omp_nthreads,
@@ -196,20 +189,15 @@ def init_dwi_preproc_wf(dwi_file, has_fieldmap=False):
     # fmt: off
     workflow.connect([
         (inputnode, dwi_derivatives_wf, [("dwi_file", "inputnode.source_file")]),
-        (inputnode, gradient_table, [("dwi_file", "dwi_file"),
-                                     ("in_bvec", "in_bvec"),
-                                     ("in_bval", "in_bval")]),
         (inputnode, dwi_reference_wf, [(("dwi_file", _aslist), "inputnode.in_files")]),
         (dwi_reference_wf, brainextraction_wf, [
             ("outputnode.epi_ref_file", "inputnode.in_file")]),
-        (gradient_table, dwi_reference_wf, [(("b0_mask", _aslist), "inputnode.t_masks")]),
         (buffernode, dwi_derivatives_wf, [
             ("dwi_reference", "inputnode.dwi_ref"),
             ("dwi_mask", "inputnode.dwi_mask"),
         ]),
         (buffernode, outputnode, [("dwi_reference", "dwi_reference"),
                                   ("dwi_mask", "dwi_mask")]),
-        (gradient_table, outputnode, [("out_rasb", "gradients_rasb")]),
     ])
     # fmt: on
 
@@ -386,64 +374,3 @@ def init_dwi_preproc_wf(dwi_file, has_fieldmap=False):
     # fmt: on
 
     return workflow
-
-
-def _get_wf_name(filename):
-    """
-    Derive the workflow name for supplied DWI file.
-
-    Examples
-    --------
-    >>> _get_wf_name("/completely/made/up/path/sub-01_dir-AP_acq-64grad_dwi.nii.gz")
-    'dwi_preproc_dir_AP_acq_64grad_wf'
-
-    >>> _get_wf_name("/completely/made/up/path/sub-01_dir-RL_run-01_echo-1_dwi.nii.gz")
-    'dwi_preproc_dir_RL_run_01_echo_1_wf'
-
-    """
-    from pathlib import Path
-
-    fname = Path(filename).name.rpartition(".nii")[0].replace("_dwi", "_wf")
-    fname_nosub = "_".join(fname.split("_")[1:])
-    return f"dwi_preproc_{fname_nosub.replace('.', '_').replace(' ', '').replace('-', '_')}"
-
-
-def _aslist(value):
-    return [value]
-
-
-def extract_entities(file_list):
-    """
-    Return a dictionary of common entities given a list of files.
-
-    Examples
-    --------
-    >>> extract_entities("sub-01/anat/sub-01_T1w.nii.gz")
-    {'subject': '01', 'suffix': 'T1w', 'datatype': 'anat', 'extension': '.nii.gz'}
-    >>> extract_entities(["sub-01/anat/sub-01_T1w.nii.gz"] * 2)
-    {'subject': '01', 'suffix': 'T1w', 'datatype': 'anat', 'extension': '.nii.gz'}
-    >>> extract_entities(["sub-01/anat/sub-01_run-1_T1w.nii.gz",
-    ...                   "sub-01/anat/sub-01_run-2_T1w.nii.gz"])
-    {'subject': '01', 'run': [1, 2], 'suffix': 'T1w', 'datatype': 'anat',
-     'extension': '.nii.gz'}
-
-    """
-    from collections import defaultdict
-
-    from bids.layout import parse_file_entities
-
-    entities = defaultdict(list)
-    for e, v in [
-        ev_pair
-        for f in listify(file_list)
-        for ev_pair in parse_file_entities(f).items()
-    ]:
-        entities[e].append(v)
-
-    def _unique(inlist):
-        inlist = sorted(set(inlist))
-        if len(inlist) == 1:
-            return inlist[0]
-        return inlist
-
-    return {k: _unique(v) for k, v in entities.items()}
