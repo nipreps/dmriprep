af6447064e58331afb7aed0cd9f18908766d22d3
Oscar Esteban <code@oscaresteban.es>
2025-10-15 11:40:15 +0200

    Improve docs version switcher deployment

--- docs/conf.py
+++ docs/conf.py
@@ -127,12 +127,21 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'version_selector': True,
+    'switcher': {
+        'json_url': 'https://nipreps.github.io/dmriprep/versions.json',
+        'version_match': version,
+    },
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Ensure the versions manifest is published alongside the built documentation.
+html_extra_path = ['_static/versions.json']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
